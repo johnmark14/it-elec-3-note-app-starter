8b1a2ddaa44f33755ce8110eb44f2ebf6e560e80
John Mark Gabrentina <johmarkgabrentina@gmail.com>
2022-10-17 15:54:28 +0800

    Added del back

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const add = require('./add')
 const read = require('./read')
-const write = require('./write')
-const update = require('./update')
+const del = require('./del')
 const present = require('./present')
 // get user input
 const cmd = process.argv
@@ -25,16 +24,13 @@ if(cmd[2] == 'read') {
     present(read())
 }
 
-if(cmd[2] == 'update') {
-    const note = {
-        id: cmd[3],
-        title: cmd[4],
-        body: cmd[5] 
-    }
-
+if(cmd[2] == 'delete') {
+    // Get old note value
     const oldNote = read()
+    // Get id
+    const id = cmd[3]
 
-    update(note, oldNote)
+    del(id, oldNote)
 
     present(read())
 }
